4f532a598473f21fe6cea39e955174beccf3dd42
poco <godiva140@gmail.com>
2023-04-16 19:54:12 +0900

    [PGS] 문자열을 정수로 바꾸기 / Lv.1
    https://school.programmers.co.kr/learn/courses/30/lessons/12925

---
+++ src/weekly15/poco/[PGS]문자열을정수로바꾸기.js
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+function solution(s) {
+  return Number(s);
+}
+
+// 매개변수
+// s : 문자열
+
+// 출력
+// 매개변수 문자열 s를 숫자로 변환한 결과를 출력하라
+
+// Number 메서드 활용해서, 문자열을 숫자로 변환
