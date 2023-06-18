252468cfb655a8185cd0423ab2bf135b00b1168c
qkdflrgs <qkdflrgs@gmail.com>
2023-06-18 11:56:00 +0900

    [LTC] 2627. Debounce / Medium

---
+++ src/weekly24/litae/leetcode/2627. Debounce.ts
@@ -0,0 +1,16 @@
+type F = (...p: any[]) => any;
+
+const debounce = (fn: F, t: number): F => {
+  let timerId;
+  return function (...args) {
+    if (timerId) clearTimeout(timerId);
+    timerId = setTimeout(() => fn(...args), t);
+  };
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
