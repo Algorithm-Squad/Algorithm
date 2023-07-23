42d56dde3850a3303fed2e14a3ba743f0cb3eead
sjuhan123 <sjuhan123@gmail.com>
2023-07-23 21:00:26 +0900

    [LTC]62. Unique Paths / medium / 1시간10분
        https://leetcode.com/problems/unique-paths/

--- src/weekly28/den/leetcode/62_UniquePaths.ts
+++ src/weekly28/den/leetcode/62_UniquePaths.ts
@@ -6,7 +6,7 @@
  */
 
 function uniquePaths(m: number, n: number): number {
-  const dp: number[][] = Array.from({ length: m }, () => Array(m).fill(1));
+  const dp: number[][] = Array.from({ length: m }, () => Array(n).fill(1));
 
   for(let i = 1; i < m; i++) {
     for(let j = 1; j < n; j++) {
