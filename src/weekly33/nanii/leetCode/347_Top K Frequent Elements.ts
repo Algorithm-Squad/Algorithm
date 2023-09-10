db12acdf00964929b8a94c1e992ec7b5d6a98674
Nanii <breadnanii@gmail.com>
2023-09-10 21:37:15 +0900

    [LTC] 347. Top K Frequent Elements / Medium

--- src/weekly33/nanii/leetCode/347_Top K Frequent Elements.ts
+++ src/weekly33/nanii/leetCode/347_Top K Frequent Elements.ts
@@ -10,6 +10,7 @@ function topKFrequent(nums: number[], k: number): number[] {
     map.set(num, map.get(num) ? map.get(num) + 1 : 1);
   });
 
+  // 주어진 배열의 숫자들을 Key로 횟수를 value로 하는 Map을 만들고, value를 기준으로 내림차순 정렬
   const sorted = [...map.entries()].sort((prev, next) => next[1] - prev[1]);
   return sorted.slice(0, k).map((num) => num[0]);
 };
